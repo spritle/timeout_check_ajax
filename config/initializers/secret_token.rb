d6abc1053194528b1114cc6e8476d1f45e1e41c7
Balaji D Loganathan <info@spritle.com>
2013-03-21 10:05:36 +0530

    40 secs timer added for posts controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timeout::Application.config.secret_token = '3b9e4a124b41eb4f254c1562d11f78e77b165bd4d99dd45af2484f0fefa19f0e55bc19ba6f9bac9d1637598509ff8f9a83ec073560f345201246e00275de8905'
